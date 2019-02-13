73295294016cd07a284f17a34c9537a8b29bde67
hongcjsgk <hongcjsgk@naver.com>
2019-02-13 14:20:30 +0900

    asdasd

--- src/project/asdasdsad.java
+++ src/project/asdasdsad.java
@@ -4,8 +4,7 @@ public class asdasdsad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("민주1");System.out.println("민주2");
-
+asdasdasdasdasdasd
 	}
 
 }
